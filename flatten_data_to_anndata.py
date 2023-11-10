a9328d673f957cddfbeb1bc1d3af2ebdc764386d
Ugur Bayindir <ubyndr@gmail.com>
2023-11-10 13:09:28 +0000

    Added script description

--- flatten_data_to_anndata.py
+++ flatten_data_to_anndata.py
@@ -1,12 +1,27 @@
+#!/usr/bin/env python3
+"""
+Script Description:
+This script processes and integrates information from a JSON file and an AnnData (Annotated Data) file,
+creating a new AnnData object that incorporates the metadata. The resulting AnnData object is then saved to a new file.
+
+Key Features:
+1. Parses command-line arguments for input JSON file, input AnnData file, and output file.
+2. Reads and processes the input JSON file and AnnData file.
+3. Updates the AnnData object with information from the JSON annotations and root keys.
+4. Writes the modified AnnData object to a specified output file.
+
+Usage:
+python script.py --json path/to/json_file.json --anndata path/to/anndata_file.h5ad --output path/to/output_file.h5ad
+"""
 import argparse
 import json
 from typing import Optional
 
 import anndata
 
-LABELSET_NAME = 'name'
+LABELSET_NAME = "name"
 
-LABELSET = 'labelset'
+LABELSET = "labelset"
 
 ANNOTATIONS = "annotations"
 
