80d7b51b51a950c09b80f2c6cbd15a9e2e1c2038
Ugur Bayindir <ubyndr@gmail.com>
2024-01-22 16:06:25 +0000

    Added a method to convert a spreadsheet to Cell Annotation Schema (CAS) JSON

---
+++ src/cas/spreadsheet_to_cas.py
@@ -0,0 +1,130 @@
+import json
+import os
+from typing import List
+
+import anndata as ad
+import cellxgene_census
+import pandas as pd
+
+from cas.file_utils import read_anndata_file
+
+
+def read_spreadsheet(file_path: str, sheet_name: str = None):
+    """
+    Read the specific sheet from the Excel file into a pandas DataFrame.
+
+    Args:
+        file_path (str): Path to the Excel file.
+        sheet_name (str, optional): Target sheet name. If not provided, reads the first sheet.
+
+    Returns:
+        tuple: Tuple containing metadata (dict), column names (list), and raw data (pd.DataFrame).
+    """
+    # Read the specific sheet or the first sheet if sheet_name is not provided
+    if sheet_name:
+        spreadsheet_df = pd.read_excel(file_path, sheet_name=sheet_name, header=None)
+    else:
+        spreadsheet_df = pd.read_excel(file_path, header=None)
+    # Extract meta data (first 8 rows)
+    meta_data = dict(spreadsheet_df.iloc[:8, 0:2].to_records(index=False))
+    # Extract column names (9th row)
+    column_names = spreadsheet_df.iloc[8, :].tolist()
+    # Extract raw data (from 10th row onwards)
+    raw_data = spreadsheet_df.iloc[9:, :]
+
+    return meta_data, column_names, raw_data
+
+
+def get_cell_ids(dataset: ad.AnnData, labelset: str, cell_label: str) -> List[str]:
+    """
+    Get cell IDs from an AnnData dataset based on a specified labelset and cell label.
+
+    Args:
+        dataset (ad.AnnData): AnnData dataset.
+        labelset (str): Labelset to filter.
+        cell_label (str): Cell label to match.
+
+    Returns:
+        List[str]: List of cell IDs.
+    """
+    return dataset.obs.index[
+        dataset.obs[labelset].str.lower() == cell_label.lower()
+    ].tolist()
+
+
+def download_and_read_dataset_with_id(dataset_id: str) -> ad.AnnData:
+    """
+    Download and read an AnnData dataset with a specified ID.
+
+    Args:
+        dataset_id (str): ID of the dataset.
+
+    Returns:
+        ad.AnnData: AnnData object.
+    """
+    anndata_file_path = f"{dataset_id}.h5ad"
+    # Check if the file already exists
+    if os.path.exists(anndata_file_path):
+        print(f"File '{anndata_file_path}' already exists. Skipping download.")
+    else:
+        cellxgene_census.download_source_h5ad(dataset_id, to_path=anndata_file_path)
+    anndata = read_anndata_file(anndata_file_path)
+    return anndata
+
+
+def spreadsheet2cas(spreadsheet_file_path: str, sheet_name: str):
+    """
+    Convert a spreadsheet to Cell Annotation Schema (CAS) JSON.
+
+    Args:
+        spreadsheet_file_path (str): Path to the spreadsheet file.
+        sheet_name (str): Target sheet name in the spreadsheet.
+    """
+    meta_data_result, column_names_result, raw_data_result = read_spreadsheet(
+        spreadsheet_file_path, sheet_name
+    )
+
+    matrix_file_id = (
+        meta_data_result["CxG LINK"].rstrip("/").split("/")[-1].split(".")[0]
+    )
+    dataset_anndata = download_and_read_dataset_with_id(matrix_file_id)
+
+    # metadata
+    cas = {
+        "matrix_file_id": matrix_file_id,
+        "cellannotation_schema_version": "TBA",
+        "cellannotation_timestamp": "TBA",
+        "cellannotation_version": "TBA",
+        "cellannotation_url": meta_data_result["CAP LINK"],
+        "author_name": "TBA",
+        "author_contact": "TBA",
+        "orcid": "TBA",
+        "annotations": [],
+    }
+
+    # #annotations
+    stripped_data_result = raw_data_result.map(
+        lambda x: x.strip() if isinstance(x, str) else x
+    )
+    for index, row in stripped_data_result.iterrows():
+        anno = {
+            "labelset": row[0],
+            "cell_label": row[1],
+            "cell_fullname": row[1],
+            "cell_ontology_term_id": row[2],
+            "cell_ontology_term": row[1],
+            "cell_ids": get_cell_ids(dataset_anndata, row[0], row[1]),
+            "rationale": row[8],
+            "rationale_dois": meta_data_result["PAPER DOI"],
+            "marker_gene_evidence": row[3],
+            "synonyms": row[6],
+            "category_fullname": row[7],
+            "category_cell_ontology_exists": "TBA",
+            "category_cell_ontology_term_id": "TBA",
+            "category_cell_ontology_term": "TBA",
+        }
+        cas.get("annotations").append(anno)
+
+    # Write the JSON data to the file
+    with open(f"{matrix_file_id}.json", "w") as json_file:
+        json.dump(cas, json_file, indent=2)
