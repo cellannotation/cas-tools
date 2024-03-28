fae6c51f80ef53c23f8982a2b9aec30181e4f824
ugur <ubyndr@gmail.com>
2024-03-28 17:06:11 +0000

    Updated anndata_to_cas.py

--- src/cas/anndata_to_cas.py
+++ src/cas/anndata_to_cas.py
@@ -3,10 +3,10 @@ from typing import Any, Dict, List
 
 from cas.file_utils import read_anndata_file
 from cas.utils.conversion_utils import (
+    add_labelsets_to_cas,
     add_parent_cell_hierarchy,
     add_parent_hierarchy_to_annotations,
     calculate_labelset,
-    generate_cas_annotations,
     generate_parent_cell_lookup,
 )
 
@@ -35,11 +35,11 @@ def anndata2cas(
 
     cas = generate_cas_metadata(dict(anndata.uns))
 
-    generate_cas_labelsets(cas, labelset_dict)
+    add_labelsets_to_cas(cas, labelset_dict)
 
     parent_cell_look_up = generate_parent_cell_lookup(anndata, labelset_dict)
 
-    generate_cas_annotations(anndata, cas, labelset_dict, parent_cell_look_up)
+    add_annotations_to_cas(cas, labelset_dict, parent_cell_look_up)
 
     if include_hierarchy:
         add_parent_cell_hierarchy(parent_cell_look_up)
@@ -48,26 +48,6 @@ def anndata2cas(
     # Write the JSON data to the file
     with open(output_file_path, "w") as json_file:
         json.dump(cas, json_file, indent=2)
-
-
-def generate_cas_labelsets(
-    cas: Dict[str, Any], labelset_dict: Dict[str, Dict[str, Any]]
-):
-    """
-    Generates CAS labelsets and updates the provided CAS dictionary.
-
-    Args:
-        cas (Dict[str, Any]): The CAS dictionary.
-        labelset_dict (Dict[str, Dict[str, Any]]): Dictionary containing labelset information (name, members, and rank).
-
-    Returns:
-        None
-    """
-    # labelsets
-    for labelset, value in labelset_dict.items():
-        cas.get("labelsets").append(
-            {"name": labelset, "description": "", "rank": str(value.get("rank"))}
-        )
 
 
 def generate_cas_metadata(uns: Dict[str, Any]) -> Dict[str, Any]:
@@ -106,3 +86,62 @@ def generate_cas_metadata(uns: Dict[str, Any]) -> Dict[str, Any]:
     # Exclude keys with None values
     cas = {k: v for k, v in cas_init.items() if v is not None}
     return cas
+
+
+def add_annotations_to_cas(
+    cas: Dict[str, Any],
+    labelset_dict: Dict[str, Any],
+    parent_cell_look_up: Dict[str, Any],
+):
+    """
+    Generates CAS annotations based on the provided AnnData object and updates the CAS
+    dictionary with new annotations. This function can optionally use a precomputed
+    parent cell lookup dictionary to enrich the annotations with hierarchical information.
+
+    Args:
+        cas (Dict[str, Any]): The CAS dictionary to be updated with annotations. Expected to have a key
+            'annotations' where new annotations will be appended.
+        labelset_dict (Dict[str, Any]): A dictionary defining labelsets and their members. This is used to match cell
+            labels with their respective metadata and annotations.
+        parent_cell_look_up (Dict[str, Any]): A precomputed dictionary containing hierarchical metadata about cell
+            labels.
+
+    Returns:
+        None: The function directly updates the `cas` dictionary with new annotations. The `parent_cell_look_up` is
+        used for enrichment and must be generated beforehand if hierarchical information is to be included.
+    """
+    for k, v in labelset_dict.items():
+        for label in v["members"]:
+            labelset = k
+            rationale = None
+            rationale_dois = None
+            marker_gene_evidence = None
+            synonyms = None
+            category_fullname = None
+            category_cell_ontology_exists = None
+            category_cell_ontology_term_id = None
+            category_cell_ontology_term = None
+
+            anno_init = {
+                "labelset": labelset,
+                "cell_label": label,
+                "cell_fullname": label,
+                "cell_set_accession": parent_cell_look_up[label]["accession"],
+                "cell_ontology_term_id": parent_cell_look_up[label][
+                    "cell_ontology_term_id"
+                ],
+                "cell_ontology_term": parent_cell_look_up[label]["cell_ontology_term"],
+                "cell_ids": list(parent_cell_look_up[label]["cell_ids"]),
+                "rationale": rationale,
+                "rationale_dois": rationale_dois,
+                "marker_gene_evidence": marker_gene_evidence,
+                "synonyms": synonyms,
+                "category_fullname": category_fullname,
+                "category_cell_ontology_exists": category_cell_ontology_exists,
+                "category_cell_ontology_term_id": category_cell_ontology_term_id,
+                "category_cell_ontology_term": category_cell_ontology_term,
+            }
+            # Exclude keys with None values
+            cas.get("annotations").append(
+                {k: v for k, v in anno_init.items() if v is not None}
+            )
