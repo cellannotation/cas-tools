e6e809f6349661f3a84a1ad78420bb926e364853
hkir-dev <huseyin_kir@yahoo.com>
2024-06-03 13:35:50 +0100

    WMB table flattening fix

--- src/cas/accession/incremental_accession_manager.py
+++ src/cas/accession/incremental_accession_manager.py
@@ -12,6 +12,8 @@ class IncrementalAccessionManager(BaseAccessionManager):
         Params:
             accession_prefix: accession_id prefix
         """
+        if accession_prefix is None:
+            accession_prefix = ""
         self.accession_prefix = accession_prefix
         self.last_accession_id = last_accession_id
         self.accession_ids = list()
@@ -27,23 +29,29 @@ class IncrementalAccessionManager(BaseAccessionManager):
         """
         if id_recommendation:
             id_recommendation = id_recommendation.replace(self.accession_prefix, "")
+            if id_recommendation.startswith("_"):
+                id_recommendation = id_recommendation[1:]
 
-        if (
-            id_recommendation
-            and id_recommendation not in self.accession_ids
-            and int(id_recommendation) > self.last_accession_id
-        ):
-            accession_id = id_recommendation
-            self.last_accession_id = int(id_recommendation)
+        if id_recommendation.isdigit():
+            if (
+                id_recommendation
+                and id_recommendation not in self.accession_ids
+                and int(id_recommendation) > self.last_accession_id
+            ):
+                accession_id = id_recommendation
+                self.last_accession_id = int(id_recommendation)
+            else:
+                id_candidate = self.last_accession_id + 1
+                while str(id_candidate) in self.accession_ids:
+                    id_candidate += 1
+                accession_id = str(id_candidate)
+                self.last_accession_id = id_candidate
         else:
-            id_candidate = self.last_accession_id + 1
-            while str(id_candidate) in self.accession_ids:
-                id_candidate += 1
-            accession_id = str(id_candidate)
-            self.last_accession_id = id_candidate
+            # non-numeric accession id
+            accession_id = id_recommendation
 
         self.accession_ids.append(accession_id)
-        if self.accession_prefix:
+        if self.accession_prefix and not accession_id.startswith(self.accession_prefix):
             accession_id = self.accession_prefix + accession_id
 
         return accession_id
