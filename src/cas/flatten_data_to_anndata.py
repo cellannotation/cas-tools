78ae173e8c00e7907aed7d6bd2f2eb10d94101d0
hkir-dev <huseyin_kir@yahoo.com>
2024-09-24 15:18:01 +0100

    linkml obj fixes

--- src/cas/flatten_data_to_anndata.py
+++ src/cas/flatten_data_to_anndata.py
@@ -131,9 +131,9 @@ def process_annotations(annotations, obs_index, parent_cell_ids, fill_na):
     accession_manager = HashAccessionManager()
     flatten_data = {}
     for ann in annotations:
-        cell_ids = ann.get(
-            CELL_IDS, parent_cell_ids.get(ann.get("cell_set_accession", []))
-        )
+        cell_ids = ann.get(CELL_IDS, [])
+        if not cell_ids:
+            cell_ids = parent_cell_ids.get(ann.get("cell_set_accession", []))
 
         ann.get("%s" % AUTHOR_ANNOTATION_FIELDS, {}).update(
             {
@@ -200,20 +200,21 @@ def generate_uns_json(input_json):
 
     for key in root_keys:
         value = input_json[key]
-        if is_list_of_strings(value):
-            uns_json[key] = ", ".join(sorted(value))
-        elif isinstance(value, str):
-            uns_json[key] = value
-        else:
-            metadata_json = {}
-            for labelset in value:
-                metadata_key = labelset.get(LABELSET_NAME, "")
-                metadata_json.update({metadata_key: {}})
-                for k, v in labelset.items():
-                    if k == LABELSET_NAME:
-                        continue
-                    metadata_json.get(metadata_key, {}).update({k: v})
-            uns_json["cellannotation_metadata"] = metadata_json
+        if value:
+            if is_list_of_strings(value):
+                uns_json[key] = ", ".join(sorted(value))
+            elif isinstance(value, str):
+                uns_json[key] = value
+            else:
+                metadata_json = {}
+                for labelset in value:
+                    metadata_key = labelset.get(LABELSET_NAME, "")
+                    metadata_json.update({metadata_key: {}})
+                    for k, v in labelset.items():
+                        if k == LABELSET_NAME:
+                            continue
+                        metadata_json.get(metadata_key, {}).update({k: v})
+                uns_json["cellannotation_metadata"] = metadata_json
 
     uns_json["cas"] = reformat_json(input_json)
 
